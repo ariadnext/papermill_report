8ef968aa21fd4494a7ce08b4f590ecae9a05bfa6
Frédéric Collonval <frederic.collonval@ariadnext.com>
2020-12-07 11:12:35 +0100

    Fix integration tests

--- e2e-tests/run_e2e.sh
+++ e2e-tests/run_e2e.sh
@@ -9,6 +9,6 @@ python3 -m playwright install
 # Ensure files written by playwright on the volume are writable by everybody
 umask a+w
 # End to end tests are stored in a separate folder
-# so that pytest does not try to import dependencies of gator
+# so that pytest does not try to import dependencies of papermill_report
 # Therefore the image controlling the frontend actions only requires playwright stack
-python3 -m pytest -m e2e e2e-tests/test_integration.py --base-url http://hub:8000
+python3 -m pytest -m e2e ./e2e-tests/test_integration.py --base-url http://hub:8000
