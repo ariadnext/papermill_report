68586a8eb73a33de82fd78e9f233b114cef6fb05
Frédéric Collonval <frederic.collonval@ariadnext.com>
2021-01-25 08:41:22 +0100

    Small e2e enhancements

--- e2e-tests/conftest.py
+++ e2e-tests/conftest.py
@@ -1,12 +1,27 @@
+import os
 from pathlib import Path
+from shutil import rmtree
 from time import sleep
 
 import pytest
 from slugify import slugify
 from playwright.sync_api import Page
 
+# Default timeout in milliseconds
+DEFAULT_TIMEOUT = 10000  # type: int
+SCREENSHOTS_DIR = Path(".playwright") / "screenshots"
+SLOW_MOTION_TIME = int(
+    os.getenv("PLAYWRIGHT_SLOWMO", "2000")  # None 0 to add some viscosity
+)  # Time to wait between action in milliseconds
+VIDEOS_DIR = Path(".playwright") / "videos"
 
-SLOW_MOTION_TIME = 2000  # Time to wait between action in milliseconds
+
+def pytest_sessionstart(session):
+    """Clean up screenshots and videos directories"""
+    if SCREENSHOTS_DIR.exists():
+        rmtree(SCREENSHOTS_DIR, ignore_errors=True)
+    if VIDEOS_DIR.exists():
+        rmtree(VIDEOS_DIR, ignore_errors=True)
 
 
 def take_screenshot(page, uid):
@@ -16,14 +31,31 @@ def take_screenshot(page, uid):
 
 
 def pytest_runtest_makereport(item, call) -> None:
-    if call.when == "call":
-        if call.excinfo is not None:
-            if "browser" in item.funcargs:
-                for cidx, context in enumerate(item.funcargs["browser"].contexts):
-                    for idx, page in enumerate(context.pages):
-                        take_screenshot(
-                            page, "-".join((slugify(item.nodeid), str(cidx), str(idx)))
-                        )
+    if "browser" in item.funcargs:
+        path, _, test_name = item.nodeid.partition("::")
+
+        if call.when == "call":
+            for cidx, context in enumerate(item.funcargs["browser"].contexts):
+                for idx, page in enumerate(context.pages):
+                    human_name = Path(path).with_suffix("") / "-".join(
+                        (slugify(test_name), str(cidx), str(idx))
+                    )
+                    # Rename the video to some more human name
+                    video = Path(page.video.path())
+                    if video.exists():
+                        new_name = video.parent / human_name.with_suffix(video.suffix)
+                        new_name.parent.mkdir(parents=True, exist_ok=True)
+                        video.rename(new_name)
+                    # Take a screenshots in case of failure
+                    if call.excinfo is not None:
+                        take_screenshot(page, human_name)
+        elif call.when == "teardown":
+            # In case there are some videos with cryptic uuid, assume they came from the current test
+            for vidx, video in enumerate(VIDEOS_DIR.glob("*.webm")):
+                human_name = Path(path).with_suffix("") / "-".join((slugify(test_name), str(vidx)))
+                new_name = video.parent / human_name.with_suffix(video.suffix)
+                new_name.parent.mkdir(parents=True, exist_ok=True)
+                video.rename(new_name)
 
 
 @pytest.fixture(scope="session")
@@ -41,14 +73,14 @@ def slow_motion():
 
 @pytest.fixture(scope="session")
 def browser_type_launch_args(browser_type_launch_args):
-    return {**browser_type_launch_args, "slowMo": SLOW_MOTION_TIME, "timeout": 5000}
+    return {**browser_type_launch_args, "slowMo": SLOW_MOTION_TIME, "timeout": DEFAULT_TIMEOUT}
 
 
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
-        "recordVideo": {"dir": str(Path(".playwright") / "videos/")},
+        "recordVideo": {"dir": str(VIDEOS_DIR)},
     }
 
 
